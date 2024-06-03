f4951b1f7db472642589182bf037b217a0704dfb
Samir6467ff <kariokie01@gmail.com>
2024-06-03 12:07:21 +0300

    Create _antiprivado.js

---
+++ plugins/_antiprivado.js
@@ -0,0 +1,16 @@
+export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner }) {
+    if (m.isBaileys && m.fromMe) return !0;
+    if (m.isGroup) return !1;
+    if (!m.message) return !0;
+    if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') || m.text.includes('serbot') || m.text.includes('jadibot')) return !0;
+
+    const chat = global.db.data.chats[m.chat];
+    const bot = global.db.data.settings[this.user.jid] || {};
+
+    if (bot.antiPrivate && !isOwner && !isROwner) {
+        await m.reply(`*[❗] مرحبًا @${m.sender.split`@`[0]}، يُمنع التحدث في الخاص مع البوت لهذا السبب سوف يتم حظرك.*\n\n*اذا كنت تريد تجربه البوت ادخل جروب البوت『 https://chat.whatsapp.com/BS48T3k00FY672UVhG3rFu 』*\n\n*اذا كنت تريد ان تكلم مطور البوت هذا رقمه 『‏U+202A『+201024480189U+202C‏*`, false, { mentions: [m.sender] });
+        await this.updateBlockStatus(m.chat, 'block');
+    }
+
+    return !1;
+}
