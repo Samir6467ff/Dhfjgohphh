922bb2205b276acbfda9837dceb8f5f203592e8a
Samir6467ff <kariokie01@gmail.com>
2024-06-04 15:25:38 +0300

    Create convertidor-toimg.js

---
+++ plugins/convertidor-toimg.js
@@ -0,0 +1,15 @@
+import {webp2png} from '../lib/webp2mp4.js';
+const handler = async (m, {conn, usedPrefix, command}) => {
+  const notStickerMessage = `*[❗𝐈𝐍𝐅𝐎❗] يجب الرد على ملصق لتحويله إلى صورة PNG بواسطة ${usedPrefix + command}*`;
+  if (!m.quoted) throw notStickerMessage;
+  const q = m.quoted || m;
+  const mime = q.mediaType || '';
+  if (!/sticker/.test(mime)) throw notStickerMessage;
+  const media = await q.download();
+  const out = await webp2png(media).catch((_) => null) || Buffer.alloc(0);
+  await conn.sendFile(m.chat, out, 'error.png', null, m);
+};
+handler.help = ['toimg (reply)'];
+handler.tags = ['sticker'];
+handler.command = ['لصوره', 'jpg', 'img'];
+export default handler;
