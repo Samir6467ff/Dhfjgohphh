dc145eba0c7bfa1ae57a9e205b3493d4dea6723a
Samir6467ff <kariokie01@gmail.com>
2024-06-05 15:33:31 +0300

    Create دبدوب.js

---
+++ plugins/دبدوب.js
@@ -0,0 +1,19 @@
+##دا الأمر بتاع دبدوب ويني ياحج سمير
+
+
+let handler = async (m, { conn, args, 
+text, usedPrefix, command }) => {
+  let too = `[❗] الأمر خطأ *🕵🏻‍♂️*\n\n *مـثــال* :\n*${usedPrefix + command}* تربو عمك`
+
+  if (!text) throw too
+  if (!text.includes('+')) throw too  
+  let [a, b] = text.split`+`   
+  let lr = (`https://api.popcat.xyz/pooh?text1=${a}&text2=${b}`)
+  conn.sendFile(m.chat, lr, 'drake.png', `تم بواسطه ✅
+  𝐵𝛩𝑺𝑺🐦‍🔥`, m)
+}
+handler.help = ['drake']
+handler.tags = ['maker']
+handler.command = ['ميم','دبدوب']
+
+export default handler
